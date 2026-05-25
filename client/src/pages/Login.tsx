b52adf979b3c0e62f881934a738734b3226d1b4d
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2026-05-26 01:01:28 +0800

    fix: login ui logo spacing

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,11 +6,11 @@ export default function Login() {
       <div className="h-screen pt-36 md:-mt-16 md:flex md:items-center md:justify-center md:flex-col">
       <header className="flex flex-col text-center items-center mx-8">
         <h1 className="text-4xl fnt-bungee">EZ<span className="text-yellow-400 fnt-bungee">|</span>Vote</h1>
-        <h1 className="mt-4 fnt-bungee bg-black text-yellow-400 px-2 w-fit">Login to proceed</h1>
+        <h1 className="mt-10 fnt-bungee bg-black text-yellow-400 px-2 w-fit">Login to proceed</h1>
         <p className="text-gray-700 mt-2">Login using your student account to make a vote.</p>
       </header>
       
-      <div className="mx-8 flex flex-col items-center gap-4 mt-16">
+      <div className="mx-8 flex flex-col items-center gap-4 mt-10">
         <div className="w-full md:w-[480px] flex flex-col">
           <label className="text-sm">Username/Student ID</label>
           <input type="text" placeholder="20260058333" className="bg-[#f7f2ee] py-2 px-4 border-2 border-black outline-none border-b-4 border-r-4 placeholder:text-gray-600" />
