608fcaa8caf08cf15c4002087ddfd60f3be1dfc6
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2026-05-26 00:54:18 +0800

    fix: input type

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,8 +4,9 @@ export default function Login() {
   return (
     <main className="hero h-screen">
       <div className="h-screen pt-36 md:-mt-16 md:flex md:items-center md:justify-center md:flex-col">
-      <header className="text-center mx-8">
+      <header className="flex flex-col text-center items-center mx-8">
         <h1 className="text-4xl fnt-bungee">EZ<span className="text-yellow-400 fnt-bungee">|</span>Vote</h1>
+        <h1 className="mt-4 fnt-bungee bg-black text-yellow-400 px-2 w-fit">Login to proceed</h1>
         <p className="text-gray-700 mt-2">Login using your student account to make a vote.</p>
       </header>
       
@@ -16,7 +17,7 @@ export default function Login() {
         </div>
         <div className="w-full md:w-[480px] flex flex-col">
           <label className="text-sm">Password</label>
-          <input type="text" placeholder="pogiako123" className="bg-[#f7f2ee] py-2 px-4 border-2 border-black outline-none border-b-4 border-r-4 placeholder:text-gray-600" />
+          <input type="password" placeholder="pogiako123" className="bg-[#f7f2ee] py-2 px-4 border-2 border-black outline-none border-b-4 border-r-4 placeholder:text-gray-600" />
         </div>
         
         <div className="flex justify-center w-full md:w-[480px] mt-2">
